57b3340be8a7a1b5260105c063d9d3391981dc5a
SuperexMack <unilexerox@gmail.com>
2025-09-19 00:54:32 +0530

    finally done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,8 @@ function App() {
 
   return (
     <>
-      <h1>This is the React App and i am your friend</h1>
+      <h1>This is the React App and i am your friend </h1>
+      <p>This is the paragraph</p>
     </>
   )
 }
